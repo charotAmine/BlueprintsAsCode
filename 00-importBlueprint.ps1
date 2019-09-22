d6524438024aca5667ae0a8c57de0ce288c68ce2
CHAROT Amine <charotamine@gmail.com>
2019-09-22 21:52:39 +0100

    add login and parameters

--- 00-importBlueprint.ps1
+++ 00-importBlueprint.ps1
@@ -1,7 +1,20 @@
 param(
     [string]$subscriptionId,
     [string]$blueprintPath,
-    [string]$blueprintName
+    [string]$blueprintName,
+    [string]$spnId,
+    [string]$spnPass,
+    [string]$tenantId
 )
+
+Install-Module -Name Az.Blueprint -AllowClobber
+if (!(Get-Module "Az.Blueprint")) {
+    throw "Module Az.Blueprint was not downloaded"
+    exit 1
+}
+
+$securePass = ConvertTo-SecureString $spnPass -AsPlainText -Force
+$credential = New-Object -TypeName pscredential -ArgumentList $spnId, $securePass
+Login-AzAccount -Credential $credential -ServicePrincipal -TenantId $tenantId
 
 Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath $blueprintPath
