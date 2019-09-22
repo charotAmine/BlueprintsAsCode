03f2de0a504a424b38b0ec2ee51ddf2a2a37b52e
CHAROT Amine <charotamine@gmail.com>
2019-09-22 22:58:18 +0100

    title upper

--- 00-importBlueprint.ps1
+++ 00-importBlueprint.ps1
@@ -14,10 +14,10 @@ $securePass = ConvertTo-SecureString $spnPass -AsPlainText -Force
 $credential = New-Object -TypeName pscredential -ArgumentList $spnId, $securePass
 Login-AzAccount -Credential $credential -ServicePrincipal -TenantId $tenantId
 
-gci "$PSScriptRoot/*.json"|%{
+gci "$blueprintPath/blueprint.json"| Where-Object{
     $NewName = (Get-Culture).TextInfo.ToTitleCase($_.Name)
     $NewFullName = join-path $_.directory -child $NewName
     $_.MoveTo($NewFullName)
 }
 
-Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath "$PSScriptRoot/$blueprintPath/" -Force
+Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath $blueprintPath -Force
